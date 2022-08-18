1034280d222d37d7ab29370ee03c68357dc9de2a
Antony <clintan_antony@cjbyte.com>
2022-08-18 19:14:54 +0530

    feature added

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { PhotoService } from '../services/photo.service';
 
 @Component({
   selector: 'app-tabs',
   templateUrl: 'tabs.page.html',
   styleUrls: ['tabs.page.scss']
 })
-export class TabsPage {
+export class TabsPage implements OnInit {
+  constructor(public photoService: PhotoService) { }
 
-  constructor() {}
+  ngOnInit(): void {
+  }
+
+  addPhotoToGallery() {
+    this.photoService.addNewToGallery();
+  }
+
 
 }
